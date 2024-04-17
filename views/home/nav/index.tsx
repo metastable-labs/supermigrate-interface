ae5697d9af41aeddf5aef5f543905372c0e71387
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-17 20:39:16 +0100

    Refactor LandingNav component in views/home/nav/index.tsx

--- views/home/nav/index.tsx
+++ views/home/nav/index.tsx
@@ -6,8 +6,12 @@ interface LandingNavProps {
 
 const LandingNav = ({ lang }: LandingNavProps) => {
   return (
-    <div>
-      <nav></nav>
+    <div className="fixed w-screen flex justify-center items-center pt-[55px] md:pt-[33px]">
+      <nav className="max-w-[648px] py-[10px] px-[14px] bg-primary-50 rounded-[12px] border-primary-100 shadow-landing-nav flex items-center justify-between gap-5">
+        <h1>Logo</h1>
+        <h1>links</h1>
+        <h1>actions</h1>
+      </nav>
     </div>
   );
 };
