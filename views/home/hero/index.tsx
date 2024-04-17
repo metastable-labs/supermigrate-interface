1dc541f59ae2363496ba4657f84019ccbe855686
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-17 20:18:51 +0100

    Add Hero component to views/home/hero/index.tsx

---
+++ views/home/hero/index.tsx
@@ -0,0 +1,11 @@
+import { Locale } from "@/config/internationalization/i18n";
+
+interface HeroProps {
+  lang: Locale;
+}
+
+const Hero = ({ lang }: HeroProps) => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
