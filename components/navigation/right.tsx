f0175626e25c45d940f2fe2290bbd22ff0f67f78
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-18 21:40:28 +0100

    Refactor Right component in components/navigation/right.tsx

--- components/navigation/right.tsx
+++ components/navigation/right.tsx
@@ -1,5 +1,39 @@
+import { ReactNode } from "react";
+
+import {
+  SelectIcon,
+  BasePrimaryIcon,
+  GitHubIcon,
+  VerifiedIcon,
+} from "@/public/icons";
+import SMClickAnimation from "../click-animation";
+
+interface SelectButtonProps {
+  icon: ReactNode;
+  secondaryIcon: ReactNode;
+  text: string;
+  isWallet: boolean;
+}
+const SelectButton = ({ icon, secondaryIcon, text }: SelectButtonProps) => {
+  return (
+    <SMClickAnimation>
+      <div className="py-1 pl-1 pr-2 flex items-center justify-center gap-[6px]">
+        <div className="flex items-center justify-center gap-2">
+          {icon}
+          <p className="text-sm font-bold">{text} </p>
+        </div>
+        <SelectIcon />
+      </div>
+    </SMClickAnimation>
+  );
+};
+
 const Right = () => {
-  return <div>right side</div>;
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Right;
