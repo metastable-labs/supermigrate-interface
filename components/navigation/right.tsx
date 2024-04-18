5b5f6c44cc4f5f50bd08a53d1549408f2b032737
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-19 00:23:53 +0100

    Refactor Right component in components/navigation/right.tsx to improve readability and maintainability

--- components/navigation/right.tsx
+++ components/navigation/right.tsx
@@ -1,81 +1,63 @@
-import { ReactNode } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
-import {
-  SelectIcon,
-  BasePrimaryIcon,
-  GitHubIcon,
-  VerifiedIcon,
-} from "@/public/icons";
-import SMClickAnimation from "../click-animation";
-import useTruncateText from "@/hooks/useTruncateText";
+import NavAction from "./nav-action";
+import { INavActions } from "./types";
+import { CloseIcon, HamIcon } from "@/public/icons";
 
-interface SelectButtonProps {
-  icon?: ReactNode;
-  secondaryIcon?: ReactNode;
-  text?: string;
-  isWallet?: boolean;
-  onClick: () => void;
-}
-
-const SelectButton = ({
-  icon,
-  secondaryIcon,
-  text,
-  isWallet,
-  onClick,
-}: SelectButtonProps) => {
-  const truncateText = useTruncateText(text || "", 4, 4);
-
-  let renderIcon;
-  if (isWallet) {
-    renderIcon = (
-      <div className="w-8 h-8 rounded-full bg-primary-150 flex items-center justify-center shadow-wallet-icon-shadow text-sm leading-none">
-        W
-      </div>
-    );
-  } else {
-    renderIcon = icon;
-  }
-
+const Ham = ({
+  menuOpen,
+  toggleMenu,
+}: {
+  menuOpen: boolean;
+  toggleMenu: () => void;
+}) => {
   return (
-    <SMClickAnimation onClick={onClick}>
-      <div className="max-w-[200px] md:max-w-[240px] bg-white rounded-[10px] border border-primary-250 shadow-nav-select-shadow py-1 pl-1 pr-2 flex items-center justify-center gap-[6px]">
-        <div className="flex items-center justify-center gap-2">
-          {renderIcon}
-          <div className="flex items-center justify-center gap-[2px]">
-            {truncateText && (
-              <span className="tracking-[-0.084px] text-sm font-medium">
-                {truncateText}
-              </span>
-            )}
-
-            {secondaryIcon && secondaryIcon}
-          </div>
-        </div>
-
-        <SelectIcon />
-      </div>
-    </SMClickAnimation>
+    <div className="md:hidden flex items-center justify-center">
+      <AnimatePresence mode="popLayout">
+        {menuOpen ? (
+          <motion.div
+            key="close"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            onClick={toggleMenu}
+          >
+            <CloseIcon />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="ham"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            onClick={toggleMenu}
+          >
+            <HamIcon />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
 
-const Right = () => {
-  const actionItems = [
-    { text: "Meister", icon: <GitHubIcon />, onClick: () => {} },
-    { icon: <BasePrimaryIcon />, onClick: () => {} },
-    {
-      text: "0x1234567890abcdef1234567890abcdef12345678",
-      isWallet: true,
-      secondaryIcon: <VerifiedIcon />,
-      onClick: () => {},
-    },
-  ];
-
+const Right = ({
+  menuOpen,
+  toggleMenu,
+  actionItems,
+}: {
+  menuOpen: boolean;
+  toggleMenu: () => void;
+  actionItems: INavActions;
+}) => {
   return (
-    <div className="flex gap-4 items-center justify-center">
-      {actionItems.map((item, index) => (
-        <SelectButton key={index} {...item} />
-      ))}
+    <div>
+      <div className="hidden md:flex gap-4 items-center justify-center">
+        {actionItems?.map((item, index) => (
+          <NavAction key={index} {...item} />
+        ))}
+      </div>
+
+      <Ham menuOpen={menuOpen} toggleMenu={toggleMenu} />
     </div>
   );
 };
