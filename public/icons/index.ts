56328411dba7370b3bd9489a291d44289eb7d7e1
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-19 00:23:30 +0100

    Add new SVG icons

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -3,12 +3,14 @@ import SMLogo from "./sm-logo.svg";
 import MigrateLinkIcon from "./migrate-link-icon.svg";
 import BridgeLinkIcon from "./bridge-link-icon.svg";
 import LiquidityLinkIcon from "./liquidity-link-icon.svg";
-import GitHubIcon from "./github-icon.svg";
-import BasePrimaryIcon from "./base-primary-icon.svg";
 import SelectIcon from "./select-icon.svg";
 import VerifiedIcon from "./verified-icon.svg";
 import HamIcon from "./ham-icon.svg";
 import CloseIcon from "./close-icon.svg";
+import GitHubDesktopIcon from "./github-desktop-icon.svg";
+import GitHubMobileIcon from "./github-mobile-icon.svg";
+import BasePrimaryDesktopIcon from "./base-primary-desktop-icon.svg";
+import BasePrimaryMobileIcon from "./base-primary-mobile-icon.svg";
 
 export {
   Logo,
@@ -16,8 +18,10 @@ export {
   MigrateLinkIcon,
   BridgeLinkIcon,
   LiquidityLinkIcon,
-  GitHubIcon,
-  BasePrimaryIcon,
+  GitHubDesktopIcon,
+  GitHubMobileIcon,
+  BasePrimaryDesktopIcon,
+  BasePrimaryMobileIcon,
   SelectIcon,
   VerifiedIcon,
   HamIcon,
