ef50f9513e6c71f65bd93b88bda1e4e5df9e9caf
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-18 20:15:14 +0100

    Add SMNavigation component to AppHome

--- app/[lang]/index.tsx
+++ app/[lang]/index.tsx
@@ -2,6 +2,7 @@ import { ReactNode, useEffect } from "react";
 
 import useLocaleActions from "@/application/locale/actions";
 import { LangParamProp } from "@/config/internationalization/i18n";
+import { SMNavigation } from "@/components";
 
 const AppHome = ({
   locale,
@@ -16,7 +17,12 @@ const AppHome = ({
     getLocale({ lang: locale.lang });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <main>{children}</main>;
+  return (
+    <main>
+      <SMNavigation />
+      {children}
+    </main>
+  );
 };
 
 export default AppHome;
