3a116b01383cd1588335d04b344848caaa24a002
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-18 21:38:30 +0100

    add new icons

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -3,5 +3,19 @@ import SMLogo from "./sm-logo.svg";
 import MigrateLinkIcon from "./migrate-link-icon.svg";
 import BridgeLinkIcon from "./bridge-link-icon.svg";
 import LiquidityLinkIcon from "./liquidity-link-icon.svg";
+import GitHubIcon from "./github-icon.svg";
+import BasePrimaryIcon from "./base-primary-icon.svg";
+import SelectIcon from "./select-icon.svg";
+import VerifiedIcon from "./verified-icon.svg";
 
-export { Logo, SMLogo, MigrateLinkIcon, BridgeLinkIcon, LiquidityLinkIcon };
+export {
+  Logo,
+  SMLogo,
+  MigrateLinkIcon,
+  BridgeLinkIcon,
+  LiquidityLinkIcon,
+  GitHubIcon,
+  BasePrimaryIcon,
+  SelectIcon,
+  VerifiedIcon,
+};
