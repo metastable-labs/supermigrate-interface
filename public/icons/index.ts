ff100a6a3702ee873acdd31b57ca98ddadb9b1b6
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-18 20:52:07 +0100

    Add new SVG icons for bridge link and liquidity link

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,3 +1,7 @@
 import Logo from "./logo.svg";
+import SMLogo from "./sm-logo.svg";
+import MigrateLinkIcon from "./migrate-link-icon.svg";
+import BridgeLinkIcon from "./bridge-link-icon.svg";
+import LiquidityLinkIcon from "./liquidity-link-icon.svg";
 
-export { Logo };
+export { Logo, SMLogo, MigrateLinkIcon, BridgeLinkIcon, LiquidityLinkIcon };
