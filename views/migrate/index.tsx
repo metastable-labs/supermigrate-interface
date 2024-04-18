577202a8e69d5743360c0a5ad5dc5abb36667669
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-18 19:29:13 +0100

    add migrate page

---
+++ views/migrate/index.tsx
@@ -0,0 +1,11 @@
+import { LangParamProp } from "@/config/internationalization/i18n";
+
+const MigrateView = ({ lang }: LangParamProp) => {
+  return (
+    <div>
+      <h1>Migrate</h1>
+    </div>
+  );
+};
+
+export default MigrateView;
