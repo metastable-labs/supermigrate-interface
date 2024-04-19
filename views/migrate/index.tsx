a0a3a805f49a8bf8473de0c4daffc218f253e621
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-19 01:27:16 +0100

    Refactor MigrateView component in views/migrate/index.tsx to use new SVG icons and improve layout

--- views/migrate/index.tsx
+++ views/migrate/index.tsx
@@ -1,11 +1,18 @@
+"use client";
 import { LangParamProp } from "@/config/internationalization/i18n";
 
-const MigrateView = ({ lang }: LangParamProp) => {
+import { DesktopTilesIcon, MobileTilesIcon } from "@/public/icons";
+
+export default function MigrateView({ lang }: LangParamProp) {
   return (
-    <div>
-      <h1>Migrate</h1>
+    <div className="pt-[123px] md:pt-[82px] text-black">
+      <div className="hidden md:flex justify-center fixed w-screen bottom-0 -z-10">
+        <DesktopTilesIcon />
+      </div>
+
+      <div className="flex md:hidden justify-center fixed w-screen bottom-0 -z-10">
+        <MobileTilesIcon />
+      </div>
     </div>
   );
-};
-
-export default MigrateView;
+}
