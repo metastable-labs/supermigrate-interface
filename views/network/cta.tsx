b6e6315a943086cbad3db51a40fde07dc5133133
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-21 04:17:05 +0100

    Refactor CTA component to use framer-motion for button animation in cta.tsx

--- views/network/cta.tsx
+++ views/network/cta.tsx
@@ -1,11 +1,17 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 const CTA = ({ title, onClick }: { title: string; onClick: () => void }) => {
   return (
-    <button
+    <motion.button
+      whileHover={{ scale: 1.04 }}
+      whileTap={{ scale: 0.9 }}
       onClick={onClick}
       className="px-3 py-2 flex items-center justify-center gap-1 bg-white rounded-lg border border-primary-1550 shadow-table-cta text-sm text-primary-1600 font-medium"
     >
       {title}
-    </button>
+    </motion.button>
   );
 };
 
