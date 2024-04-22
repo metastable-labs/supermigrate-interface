28a1f99b20ff3df2582d6122c04432d3994a1f0b
meisterjustice <eziefulejustice@gmail.com>
2024-04-22 18:43:29 +0100

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ["@svgr/webpack"],
-      });
-  
-      return config;
-    },
-    images: {
-      domains: [""],
-    },
-  };
-  
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  images: {
+    domains: ["w7.pngwing.com"],
+  },
+};
+
 //   module.exports = nextConfig;
 export default nextConfig;
-  
