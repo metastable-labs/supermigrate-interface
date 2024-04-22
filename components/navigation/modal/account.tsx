69e34d7e00775e2b1e23fd3f80ec8a957d0a2125
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-22 15:02:00 +0100

    Refactor account modal component to display username dynamically in account.tsx

--- components/navigation/modal/account.tsx
+++ components/navigation/modal/account.tsx
@@ -4,18 +4,25 @@ import { DisconnectIcon } from "@/public/icons";
 const AccountModal = ({ close }: { close: () => void }) => {
   const action = () => {};
 
+  const modalTitle = `@${"Meistergit"}`;
+
   return (
-    <SMClickAnimation
-      className="flex-1 bg-primary-150 flex items-center justify-center p-3 rounded-xl"
-      onClick={action}
-    >
-      <div className="flex flex-col items-center justify-center gap-2">
-        <DisconnectIcon />
-        <span className="text-primary-50 text-[14px] leading-[21.7px] font-medium text-center">
-          Disconnect
-        </span>
-      </div>
-    </SMClickAnimation>
+    <div className="flex flex-col gap-6 min-w-[300px] md:min-w-80">
+      <h1 className="text-[20px] leading-[30px] text-primary-1750 text-left font-medium">
+        {modalTitle}
+      </h1>
+      <SMClickAnimation
+        className="flex-1 bg-primary-150 flex items-center justify-center p-3 rounded-xl"
+        onClick={action}
+      >
+        <div className="flex flex-col items-center justify-center gap-2">
+          <DisconnectIcon />
+          <span className="text-primary-50 text-[14px] leading-[21.7px] font-medium text-center">
+            Disconnect
+          </span>
+        </div>
+      </SMClickAnimation>
+    </div>
   );
 };
 
