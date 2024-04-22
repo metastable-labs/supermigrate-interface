337ab4f021ab5c57666d68840bc53d89fd7e2538
meisterjustice <eziefulejustice@gmail.com>
2024-04-22 18:46:01 +0100

    update component exports

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,14 @@ import SMContainer from "./container";
 import SMNavigation from "./navigation";
 import SMClickAnimation from "./click-animation";
 import SMModal from "./modal";
+import SMTable from "./table";
+import SMButton from "./button";
 
-export { SMContainer, SMClickAnimation, SMNavigation, SMModal };
+export {
+  SMContainer,
+  SMClickAnimation,
+  SMNavigation,
+  SMModal,
+  SMTable,
+  SMButton,
+};
