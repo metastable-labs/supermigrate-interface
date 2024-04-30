a37aa91d8124dbe1e20f7be43c62010ada1b93bb
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-30 23:34:13 +0100

    Update network backgrounds in app.tsx

--- app/[lang]/migrate/[network]/app.tsx
+++ app/[lang]/migrate/[network]/app.tsx
@@ -1,19 +1,27 @@
 "use client";
 import { ReactNode, useEffect } from "react";
 
-import { Network } from "@/components/button/types";
 import { DesktopTilesIcon, MobileTilesIcon } from "@/public/icons";
+import { Network } from "@/config/rainbow/rainbowkit";
 
 interface PageProps {
   params: { network: Network };
   children: ReactNode;
 }
 
+const defaultGradient =
+  "linear-gradient(180deg, #FFF 0%, #FCFCFC 43%, #CCCCCC 100%)";
+
 const networkBackgrounds: Record<Network, string> = {
   base: "linear-gradient(180deg, #FFF 0%, #FCFCFC 43%, #0052FF 100%)",
   optimism: "linear-gradient(180deg, #FFF 0%, #FCFCFC 43%, #FF1636 100%)",
   mode: "linear-gradient(180deg, #FFF 0%, #FCFCFC 43%, #CDFF00 100%)",
   scroll: "linear-gradient(180deg, #FFF 0%, #FCFCFC 43%, #F1C087 100%)",
+  linea: defaultGradient,
+  zora: defaultGradient,
+  "world-chain": defaultGradient,
+  degen: defaultGradient,
+  pgn: defaultGradient,
 };
 
 const App = ({ params, children }: PageProps) => {
