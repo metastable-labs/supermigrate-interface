50aac29c95a2b807703923f42186f977be587bef
Justice Eziefule <eziefulejustice@gmail.com>
2024-04-30 23:38:01 +0100

    initiate add liquidity modal

---
+++ views/liquidity/add.tsx
@@ -0,0 +1,12 @@
+const Add = () => {
+  let headerText = "Add Liquidity";
+  return (
+    <div className="w-[343px] md:w-[448px] p-1 flex flex-col gap-6 items-start rounded-2xl">
+      <h1 className="text-primary-50 text-[24px] leading-[37.2px] font-medium">
+        {headerText}
+      </h1>
+    </div>
+  );
+};
+
+export default Add;
