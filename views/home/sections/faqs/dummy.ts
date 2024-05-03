d6b48245f76751defd2e3accce930f2a2b2fb051
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-03 15:14:49 +0100

    Refactor FAQs component and add dummy data

---
+++ views/home/sections/faqs/dummy.ts
@@ -0,0 +1,59 @@
+const faqs = [
+  {
+    question:
+      "What are the main benefits of using Supermigrate for blockchain migration?",
+    answer:
+      "Supermigrate offers streamlined processes, expert support, and tools that simplify the transition to Layer 2 solutions, enhancing scalability and efficiency for your blockchain projects.",
+  },
+  {
+    question: "Does Supermigrate support migration from any blockchain?",
+    answer:
+      "Supermigrate is designed to support a wide range of blockchains, allowing users to seamlessly migrate from popular base layers to advanced Layer 2 solutions.",
+  },
+  {
+    question: "How secure is the migration process with Supermigrate?",
+    answer:
+      "Supermigrate prioritizes security with end-to-end encryption and continuous monitoring to ensure safe and reliable migration of your blockchain assets and data.",
+  },
+  {
+    question:
+      "Can Supermigrate help with smart contract deployment on Layer 2?",
+    answer:
+      "Yes, Supermigrate facilitates not only migration but also the deployment and management of smart contracts on Layer 2 platforms to streamline operations and reduce costs.",
+  },
+  //   {
+  //     question:
+  //       "Is there downtime associated with migrating to Layer 2 via Supermigrate?",
+  //     answer:
+  //       "Supermigrate minimizes downtime by providing robust tools and services that ensure a smooth transition without interrupting your blockchain operations.",
+  //   },
+  //   {
+  //     question: "What types of customer support does Supermigrate offer?",
+  //     answer:
+  //       "Supermigrate offers comprehensive customer support including 24/7 technical support, detailed documentation, and community forums to assist you throughout the migration process.",
+  //   },
+  //   {
+  //     question:
+  //       "How does Supermigrate handle data integrity during the migration process?",
+  //     answer:
+  //       "Data integrity is maintained through rigorous checks, balances, and the use of advanced cryptographic techniques to ensure that all data remains intact and secure during migration.",
+  //   },
+  //   {
+  //     question: "Can I trial Supermigrate before committing to a full migration?",
+  //     answer:
+  //       "Yes, Supermigrate offers a trial period where you can test out the platform’s capabilities and ensure it meets your needs before undergoing a full migration.",
+  //   },
+  //   {
+  //     question:
+  //       "How long does a typical migration to Layer 2 take with Supermigrate?",
+  //     answer:
+  //       "The duration of migration depends on the complexity and size of the assets being migrated, but Supermigrate is engineered to perform migrations efficiently and swiftly.",
+  //   },
+  //   {
+  //     question: "Does Supermigrate offer analytics and reporting post-migration?",
+  //     answer:
+  //       "Post-migration, Supermigrate provides detailed analytics and reporting tools to help you understand the impact of migration and optimize your Layer 2 operations.",
+  //   },
+];
+
+export { faqs };
---
+++ views/home/sections/faqs/index.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useState } from "react";
+
+import { faqs } from "./dummy";
+import { SMFAQ } from "@/components";
+import { LinkRightArrow } from "@/public/icons";
+
+const FAQs = () => {
+  const [current, setCurrent] = useState(0);
+
+  return (
+    <section id="faq" className="mt-[58px] lg:mt-[96px] lg:mb-[96px]">
+      <div className="flex flex-col md:flex-row gap-16">
+        <div className="self-stretch">
+          <h3 className="text-[24px] leading-[44px] md:text-[36px] text-primary-300 font-medium tracking-[-0.48px] md:tracking-[-0.72px]">
+            Frequently Asked Questions
+          </h3>
+
+          <h4 className="text-sm leading-normal md:text-lg text-[#475467]">
+            Everything you need to know about Supermigrate and . Can’t find the
+            answer you’re looking for?{" "}
+            <span className="text-primary-2450 underline underline-offset-2">
+              See our Support page
+            </span>
+          </h4>
+        </div>
+
+        <div className="flex flex-col gap-4 items-start">
+          {faqs.map(({ answer, question }, index) => (
+            <SMFAQ
+              key={index}
+              question={question}
+              answer={answer}
+              current={current === index}
+              onClick={() => setCurrent(index)}
+            />
+          ))}
+
+          <a
+            href=""
+            className="px-8 py-6 self-stretch flex items-center justify-start gap-1 text-left rounded-2xl min-w-full text-primary-2450 text-lg font-medium"
+          >
+            See more FAQ{" "}
+            <LinkRightArrow color="#C2540A" width={24} height={24} />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FAQs;
