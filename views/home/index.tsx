ce6b77023c898939bcf7722b6da83c23e4f3fd07
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-03 12:30:01 +0100

    Refactor home view to use Testimonials component instead of TweetSection

--- views/home/index.tsx
+++ views/home/index.tsx
@@ -12,10 +12,10 @@ import SectionOne from "./sections/SectionOne";
 import SectionTwo from "./sections/SectionTwo";
 import SectionThree from "./sections/SectionThree";
 import SectionFour from "./sections/SectionFour";
-import TweetSection from "./sections/TweetSection";
 import FAQs from "./sections/FAQs";
 import Footer from "./sections/Footer";
 import LandingHeader from "./header";
+import Testimonials from "./sections/testimonials";
 
 export default function HomeView({ lang }: LangParamProp) {
   const { userState, locale } = useSystemFunctions();
@@ -43,7 +43,7 @@ export default function HomeView({ lang }: LangParamProp) {
         <SectionTwo />
         <SectionThree />
         <SectionFour />
-        <TweetSection />
+        <Testimonials />
         <FAQs />
         {/* < */}
       </SMContainer>
