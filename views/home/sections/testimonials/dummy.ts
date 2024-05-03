7072f0b23500905a3d6353fc74646a90384d301b
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-03 12:33:50 +0100

    Add testimonials dummy data

---
+++ views/home/sections/testimonials/dummy.ts
@@ -0,0 +1,188 @@
+import { ISMTestimonial } from "@/components/testimonial/type";
+
+const testimonials: ISMTestimonial[] = [
+  {
+    name: "Alex Johnson",
+    twitterUrl: "https://twitter.com/alexjohnson",
+    imageUrl: "https://i.pravatar.cc/150?img=1",
+    content: "Fantastic experience with this product. Highly recommended!",
+  },
+  {
+    name: "Maria Lopez",
+    twitterUrl: "https://twitter.com/marialopez",
+    imageUrl: "https://i.pravatar.cc/150?img=2",
+    content: "This service changed my life! Absolutely incredible!",
+  },
+  {
+    name: "Chris Green",
+    twitterUrl: "https://twitter.com/chrisgreen",
+    imageUrl: "https://i.pravatar.cc/150?img=3",
+    content:
+      "Great value for the price, and the customer service was excellent!",
+  },
+  {
+    name: "Emma Stone",
+    twitterUrl: "https://twitter.com/emmastone",
+    imageUrl: "https://i.pravatar.cc/150?img=4",
+    content: "Truly a five-star experience from start to finish.",
+  },
+  {
+    name: "David Smith",
+    twitterUrl: "https://twitter.com/davidsmith",
+    imageUrl: "https://i.pravatar.cc/150?img=5",
+    content: "The quality is top-notch. I would definitely buy again.",
+  },
+  {
+    name: "Sophia Wilson",
+    twitterUrl: "https://twitter.com/sophiawilson",
+    imageUrl: "https://i.pravatar.cc/150?img=6",
+    content: "This is the best thing I've ever purchased online.",
+  },
+  {
+    name: "Ethan Brown",
+    twitterUrl: "https://twitter.com/ethanbrown",
+    imageUrl: "https://i.pravatar.cc/150?img=7",
+    content: "Couldn't be happier with the results!",
+  },
+  {
+    name: "Olivia Miller",
+    twitterUrl: "https://twitter.com/oliviamiller",
+    imageUrl: "https://i.pravatar.cc/150?img=8",
+    content: "I tell all my friends about this. It's that good.",
+  },
+  {
+    name: "James Wilson",
+    twitterUrl: "https://twitter.com/jameswilson",
+    imageUrl: "https://i.pravatar.cc/150?img=9",
+    content: "A must-have for anyone considering this type of product.",
+  },
+  {
+    name: "Isabella Davis",
+    twitterUrl: "https://twitter.com/isabelladavis",
+    imageUrl: "https://i.pravatar.cc/150?img=10",
+    content: "Exceeded all my expectations.",
+  },
+  {
+    name: "Michael Garcia",
+    twitterUrl: "https://twitter.com/michaelgarcia",
+    imageUrl: "https://i.pravatar.cc/150?img=11",
+    content: "Impressive performance. Nothing else comes close.",
+  },
+  {
+    name: "Emily Rodriguez",
+    twitterUrl: "https://twitter.com/emilyrodriguez",
+    imageUrl: "https://i.pravatar.cc/150?img=12",
+    content: "I was skeptical at first, but I'm totally converted now.",
+  },
+  {
+    name: "Matthew Martinez",
+    twitterUrl: "https://twitter.com/matthewmartinez",
+    imageUrl: "https://i.pravatar.cc/150?img=13",
+    content: "Well worth the investment. I am thoroughly impressed.",
+  },
+  {
+    name: "Ava Anderson",
+    twitterUrl: "https://twitter.com/avaanderson",
+    imageUrl: "https://i.pravatar.cc/150?img=14",
+    content: "Fantastic results! I can't recommend this enough.",
+  },
+  {
+    name: "Daniel Thomas",
+    twitterUrl: "https://twitter.com/danielthomas",
+    imageUrl: "https://i.pravatar.cc/150?img=15",
+    content: "This product is revolutionary! It's a game-changer.",
+  },
+  {
+    name: "Mia Taylor",
+    twitterUrl: "https://twitter.com/miataylor",
+    imageUrl: "https://i.pravatar.cc/150?img=16",
+    content: "Very happy with the outcome. It was super easy to use.",
+  },
+  {
+    name: "Jacob Moore",
+    twitterUrl: "https://twitter.com/jacobmoore",
+    imageUrl: "https://i.pravatar.cc/150?img=17",
+    content: "A reliable and efficient service that I'd use again.",
+  },
+  {
+    name: "Madison Lee",
+    twitterUrl: "https://twitter.com/madisonlee",
+    imageUrl: "https://i.pravatar.cc/150?img=18",
+    content: "The customer support was fantastic. Very helpful.",
+  },
+  {
+    name: "Jayden Perez",
+    twitterUrl: "https://twitter.com/jaydenperez",
+    imageUrl: "https://i.pravatar.cc/150?img=19",
+    content: "The product arrived on time and exceeded my expectations.",
+  },
+  {
+    name: "Charlotte White",
+    twitterUrl: "https://twitter.com/charlottewhite",
+    imageUrl: "https://i.pravatar.cc/150?img=20",
+    content: "The design is sleek, and the functionality is unbeatable.",
+  },
+  {
+    name: "Noah Harris",
+    twitterUrl: "https://twitter.com/noahharris",
+    imageUrl: "https://i.pravatar.cc/150?img=21",
+    content: "I'm impressed with the quality and performance.",
+  },
+
+  {
+    name: "Lily Clark",
+    twitterUrl: "https://twitter.com/lilyclark",
+    imageUrl: "https://i.pravatar.cc/150?img=22",
+    content: "Great job on this product. It does everything it promises.",
+  },
+  {
+    name: "Benjamin Lewis",
+    twitterUrl: "https://twitter.com/benjaminlewis",
+    imageUrl: "https://i.pravatar.cc/150?img=23",
+    content: "I would definitely recommend this to anyone looking for quality.",
+  },
+  {
+    name: "Zoey Robinson",
+    twitterUrl: "https://twitter.com/zoeyrobinson",
+    imageUrl: "https://i.pravatar.cc/150?img=24",
+    content: "This is the best version of this product I've ever used.",
+  },
+  {
+    name: "William Walker",
+    twitterUrl: "https://twitter.com/williamwalker",
+    imageUrl: "https://i.pravatar.cc/150?img=25",
+    content: "Top-notch quality and service.",
+  },
+  {
+    name: "Hannah Young",
+    twitterUrl: "https://twitter.com/hannahyoung",
+    imageUrl: "https://i.pravatar.cc/150?img=26",
+    content: "Super satisfied with my purchase. Five stars!",
+  },
+  {
+    name: "Lucas Allen",
+    twitterUrl: "https://twitter.com/lucasallen",
+    imageUrl: "https://i.pravatar.cc/150?img=27",
+    content: "A great find! This product exceeded my expectations.",
+  },
+  {
+    name: "Sophie King",
+    twitterUrl: "https://twitter.com/sophieking",
+    imageUrl: "https://i.pravatar.cc/150?img=28",
+    content: "Really impressed with the fast delivery and the quality.",
+  },
+  {
+    name: "Jack Wright",
+    twitterUrl: "https://twitter.com/jackwright",
+    imageUrl: "https://i.pravatar.cc/150?img=29",
+    content: "They really thought of everything with this product.",
+  },
+  {
+    name: "Scarlett Martin",
+    twitterUrl: "https://twitter.com/scarlettmartin",
+    imageUrl: "https://i.pravatar.cc/150?img=30",
+    content: "Absolutely flawless performance from this product.",
+  },
+];
+
+export { testimonials };
