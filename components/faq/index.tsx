a1620a44c4b65d0ab7854ab766bca624effdbc3a
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-05 23:28:38 +0100

    Update FAQ component styling and behavior

--- components/faq/index.tsx
+++ components/faq/index.tsx
@@ -5,16 +5,16 @@ import classNames from 'classnames';
 const SMFAQ = ({ answer, question, current, onClick }: ISMFAQ) => {
   return (
     <div className={classNames('px-8 py-6 flex flex-col self-stretch items-start rounded-2xl md:w-[440px] lg:w-[576px]', { 'gap-2 bg-primary-1450 transition-colors duration-300': current })}>
-      <div className="w-full flex justify-between items-center">
+      <div onClick={onClick} className="w-full flex justify-between items-center cursor-pointer">
         <p className="w-full text-primary-300 text-[16px] leading-[24px] md:text-lg font-medium">{question}</p>
 
         <AnimatePresence mode="popLayout">
           {current ? (
-            <motion.div key="close" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="cursor-pointer" onClick={onClick}>
+            <motion.div key="close" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
               <MinusIcon color="black" height={24} width={24} />
             </motion.div>
           ) : (
-            <motion.div key="open" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="cursor-pointer" onClick={onClick}>
+            <motion.div key="open" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
               <PlusIcon color="black" height={24} width={24} />
             </motion.div>
           )}
