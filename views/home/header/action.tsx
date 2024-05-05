614eefa7efc4b518d25bc9f0a841e6b63e038d97
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-05 07:36:16 +0100

    Update header menu styles and class name in action.tsx

--- views/home/header/action.tsx
+++ views/home/header/action.tsx
@@ -7,7 +7,7 @@ const Action = ({ onClick }: { onClick: () => void }) => {
       whileHover={{ scale: 1.04 }}
       whileTap={{ scale: 0.9 }}
       onClick={onClick}
-      className="px-3 py-2 flex items-center justify-center gap-1 bg-white rounded-lg border border-primary-1300 shadow-table-cta"
+      className="px-3 py-2 flex items-center justify-center gap-1 bg-white rounded-base border border-primary-1300 shadow-table-cta"
     >
       <span className="text-sm text-primary-50 font-medium px-[2px]">
         Launch app
