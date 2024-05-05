cddf25dab2da53e82bd2565d1745c69e9b764ee9
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-05 23:15:07 +0100

    Add GitHubMobileIcon to AccountModal component

--- components/navigation/modal/account.tsx
+++ components/navigation/modal/account.tsx
@@ -1,6 +1,6 @@
 import SMClickAnimation from '@/components/click-animation';
 import useSystemFunctions from '@/hooks/useSystemFunctions';
-import { DisconnectIcon } from '@/public/icons';
+import { DisconnectIcon, GitHubMobileIcon } from '@/public/icons';
 
 const AccountModal = ({ close }: { close: () => void }) => {
   const { userState } = useSystemFunctions();
@@ -10,7 +10,10 @@ const AccountModal = ({ close }: { close: () => void }) => {
 
   return (
     <div className="flex flex-col gap-6 min-w-[300px] md:min-w-80">
-      <h1 className="text-[20px] leading-[30px] text-primary-1750 text-left font-medium">{modalTitle}</h1>
+      <h1 className="text-[20px] leading-[30px] text-primary-1750 text-left font-medium flex items-center gap-1">
+        <GitHubMobileIcon />
+        {modalTitle}
+      </h1>
       <SMClickAnimation className="flex-1 bg-primary-150 flex items-center justify-center p-3 rounded-xl" onClick={action}>
         <div className="flex flex-col items-center justify-center gap-2">
           <DisconnectIcon />
