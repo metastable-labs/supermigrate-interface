aad2ad311f00c8e7c12f9a232ee8ed30df06001d
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-05 09:50:32 +0100

    Refactor testimonial component styles and update Twitter link in index.tsx

--- components/testimonial/index.tsx
+++ components/testimonial/index.tsx
@@ -20,7 +20,7 @@ const SMTestimonial = ({
   const twitterUsername = getTwitterUsername(twitterUrl);
 
   return (
-    <div className="mx-[15px] py-6 px-8 border border-[#CDD0D5] flex flex-col gap-6 rounded-lg w-[293px] md:w-[429px] min-h-full">
+    <div className="mx-[15px] py-6 px-8 border border-primary-250 flex flex-col gap-6 rounded-base w-[293px] md:w-[429px] min-h-full">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2.5">
           <Image
@@ -31,9 +31,13 @@ const SMTestimonial = ({
             className="rounded-full"
           />
           <div>
-            <h3 className="text-sm font-medium">{name}</h3>
-            <a href={twitterUrl} target="_blank">
-              <h4 className="text-[#666D80] text-sm">@{twitterUsername}</h4>
+            <h3 className="text-sm font-medium text-primary-3700">{name}</h3>
+            <a
+              href={twitterUrl}
+              target="_blank"
+              className="text-primary-3650 text-sm"
+            >
+              @{twitterUsername}
             </a>
           </div>
         </div>
@@ -47,7 +51,7 @@ const SMTestimonial = ({
         </a>
       </div>
 
-      <p className="text-[#666D80] text-[10px] leading-[15px] md:text-base">
+      <p className="text-primary-3650 text-[10px] leading-[15px] md:text-base">
         {content}
       </p>
     </div>
