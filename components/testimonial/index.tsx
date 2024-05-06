d74c2ee24b83356b3a01f8ee943626efb8ffdfb3
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-06 16:33:37 +0100

    Update testimonial component to open Twitter link in a new tab

--- components/testimonial/index.tsx
+++ components/testimonial/index.tsx
@@ -15,24 +15,21 @@ const SMTestimonial = ({ content, imageUrl, name, twitterUrl }: ISMTestimonial) 
   const twitterUsername = getTwitterUsername(twitterUrl);
 
   return (
-    <div className="mx-[15px] py-6 px-8 border border-primary-250 flex flex-col gap-6 rounded-base w-[293px] md:w-[429px] min-h-full">
+    <a target="_blank" href={twitterUrl} className="mx-[15px] py-6 px-8 border border-primary-250 flex flex-col gap-6 rounded-base w-[293px] md:w-[429px] min-h-full">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2.5">
           <Image src={imageUrl} alt={`${name}'s avatar`} height={40} width={40} className="rounded-full" />
           <div>
             <h3 className="text-sm font-medium text-primary-3700">{name}</h3>
-            <a href={twitterUrl} target="_blank" className="text-primary-3650 text-sm">
-              @{twitterUsername}
-            </a>
+            <span className="text-primary-3650 text-sm">@{twitterUsername}</span>
           </div>
         </div>
-        <a href={twitterUrl} target="_blank">
-          <Image src="https://res.cloudinary.com/palmlight/image/upload/v1714329710/twitter-x-line_iekg46.svg" height={24} width={24} alt="Twitter" />
-        </a>
+
+        <Image src="https://res.cloudinary.com/palmlight/image/upload/v1714329710/twitter-x-line_iekg46.svg" height={24} width={24} alt="Twitter" />
       </div>
 
       <p className="text-primary-3650 text-[10px] leading-[15px] md:text-base">{content}</p>
-    </div>
+    </a>
   );
 };
 
