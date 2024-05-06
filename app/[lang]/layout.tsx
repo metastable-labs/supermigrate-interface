33816ced919b9b2535fdc87c4aa30f56f9d50ae7
meisterjustice <eziefulejustice@gmail.com>
2024-05-06 19:15:11 +0100

    add meta tag

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -11,6 +11,36 @@ interface RootProps {
 export const metadata: Metadata = {
   title: 'Supermigrate',
   description: 'Your gateway to L2s',
+  keywords: ['base', 'migrate', 'erc20', 'tokens', 'migrate ERC20 tokens to base network', 'base network', 'base migrate', 'erc20 token to base network', 'migrate erc20 tokens'],
+  applicationName: 'Supermigrate',
+  authors: [{ name: 'Justice Eziefule', url: 'https://github.com/meisterjustice' }],
+  openGraph: {
+    title: 'Supermigrate',
+    description: 'Your gateway to L2s',
+    url: 'https://supermigrate.xyz',
+    type: 'website',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/djzeufu4j/image/upload/v1715019214/favicon_ihu7jb.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    siteName: 'supermigrate.xyz',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: 'https://supermigrate.xyz',
+    title: 'Supermigrate',
+    description: 'Your gateway to L2s',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/djzeufu4j/image/upload/v1715019214/favicon_ihu7jb.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ params, children }: Readonly<RootProps>) {
