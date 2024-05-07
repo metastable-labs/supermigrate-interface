1355277189ce5693efe911e97cb7a0e496486975
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-07 11:57:06 +0100

    Fix welcome modal close button functionality and add localStorage support

--- views/migrate/index.tsx
+++ views/migrate/index.tsx
@@ -1,14 +1,23 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { SMBottomComment, SMContainer, SMWelcome } from '@/components';
 import { LangParamProp } from '@/config/internationalization/i18n';
 import SelectionComponent from './selection';
 
 export default function MigrateView({ lang }: LangParamProp) {
-  const [showWelcome, setShowWelcome] = useState(true);
+  const [showWelcome, setShowWelcome] = useState(false);
 
   const closeWelcome = () => setShowWelcome(false);
+
+  useEffect(() => {
+    const dontShowAgain = localStorage.getItem('dontShowWelcome');
+    if (dontShowAgain === 'true') {
+      setShowWelcome(false);
+    } else {
+      setShowWelcome(true);
+    }
+  }, []);
   return (
     <div className="pb-28 lg:pb-0">
       <SMContainer>
