c3b56af713513162baf8f25ca4d704ca10bd3a57
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-09 20:29:50 +0100

    Add Liquidity page component to [lang]/[network]/liquidity/page.tsx

---
+++ app/[lang]/[network]/liquidity/page.tsx
@@ -0,0 +1,6 @@
+import { LangParamProp } from '@/config/internationalization/i18n';
+import { LiquidityView } from '@/views';
+
+export default function Liquidity({ params: { lang } }: { params: LangParamProp }) {
+  return <LiquidityView lang={lang} />;
+}
