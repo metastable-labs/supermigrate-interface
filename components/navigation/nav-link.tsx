c3076c962360c9dcda9809413db9edbbe0b87aa2
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-09 20:54:24 +0100

    Refactor NavLink component to remove network-specific logic

--- components/navigation/nav-link.tsx
+++ components/navigation/nav-link.tsx
@@ -13,20 +13,9 @@ interface NavLinkProps {
 }
 
 const NavLink = ({ title, icon, href, isActive, fullWidth }: NavLinkProps) => {
-  const chainId = useChainId();
-
-  const connectedNetwork = networks.find((chain) => chain.chainId === chainId);
-
-  const network = connectedNetwork?.variant;
-
-  let link = href;
-  if (href === '/migrate' && network) {
-    link = `${href}/${network}`;
-  }
-
   return (
     <Link
-      href={link}
+      href={href}
       className={classNames('py-2 pl-2 pr-3 rounded-lg transition-all duration-200 flex gap-2 items-center justify-start', {
         'bg-primary-150 text-primary-50': isActive,
         'bg-white text-primary-200': !isActive,
