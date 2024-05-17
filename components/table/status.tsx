46f8be2ee058c9226ad1a78a40855480f745510a
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-17 17:35:30 +0100

    Refactor Status component to use localized strings for status text

--- components/table/status.tsx
+++ components/table/status.tsx
@@ -1,26 +1,29 @@
-import { DoubleCheckIcon, MergedIcon } from '@/public/icons';
-import { PullStatus } from './types';
+import useSystemFunctions from '@/hooks/useSystemFunctions';
+import { StatusProps } from './types';
 import classNames from 'classnames';
 
-const Status = ({ status }: { status?: PullStatus }) => {
+const Status = ({ status }: StatusProps) => {
+  const { locale } = useSystemFunctions();
+  const { status: statusText } = locale.components.table.body;
+
   let text;
   switch (status) {
     case 'merged':
-      text = 'Success';
+      text = statusText.success;
       break;
     case 'pending':
-      text = 'Pending';
+      text = statusText.pending;
       break;
     case 'failed':
-      text = 'Failed';
+      text = statusText.failed;
       break;
     default:
-      text = 'Success';
+      text = statusText.success;
       break;
   }
   return (
     <div
-      className={classNames('px-2.5 py-0.5 flex items-center justify-center border rounded-2xl max-w-20', {
+      className={classNames('px-2.5 py-0.5 flex items-center justify-center border rounded-2xl min-w-20 max-w-fit', {
         'border-primary-3100 text-primary-3100 text-sm': status === 'merged',
         'border-primary-3150 text-primary-3150 text-sm': status === 'pending',
         'border-primary-3200 text-primary-3200 text-sm': status === 'failed',
