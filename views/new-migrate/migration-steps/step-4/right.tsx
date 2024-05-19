34657def93b749880f44fd54eb9b33b00ee00f4f
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-19 02:49:55 +0100

    Refactor Step4 component to use localized strings for link texts, subtitle, and title

--- views/new-migrate/migration-steps/step-4/right.tsx
+++ views/new-migrate/migration-steps/step-4/right.tsx
@@ -1,20 +1,12 @@
 import { LinkRightArrow, MergedSecondaryIcon, XIcon } from '@/public/icons';
 import { VerifyStepProps } from './types';
-
-const verifySteps: VerifyStepProps[] = [
-  {
-    text: 'Make the tweet below from the account of the twitter URL you provided',
-    variant: 'x',
-  },
-  {
-    text: 'Go to the Pull requests and add a comment saying approved with a link to the tweet',
-    variant: 'git',
-  },
-];
+import useSystemFunctions from '@/hooks/useSystemFunctions';
 
 const twitterUrl = `https://twitter.com/intent/tweet?text=@supermigrate`;
 
 const VerifyStep = ({ text, variant = 'x' }: VerifyStepProps) => {
+  const { locale } = useSystemFunctions();
+  const { linkText } = locale.newMigration.step4.right;
   return (
     <div className="self-stretch flex flex-col md:flex-row gap-3 md:gap-4 justify-center items-start min-w-[320px] max-w-[560px]">
       <div className="p-3 bg-primary-150 w-12 h-12 rounded-[10px] flex justify-center items-center shadow-sm">{variant === 'git' ? <MergedSecondaryIcon color="#31353F" /> : <XIcon />}</div>
@@ -25,7 +17,7 @@ const VerifyStep = ({ text, variant = 'x' }: VerifyStepProps) => {
             href={twitterUrl}
             target="_blank"
             className="text-primary-3350 text-[14px] leading-[18.2px] tracking-[-0.14px] underline underline-offset-4 flex items-center justify-center gap-2 pb-[3px]">
-            Verifying migration on @supermigrate
+            {linkText}
             <LinkRightArrow color="#6B8000" />
           </a>
         )}
@@ -34,17 +26,33 @@ const VerifyStep = ({ text, variant = 'x' }: VerifyStepProps) => {
   );
 };
 
-const Right = () => (
-  <div className="flex flex-col item-center gap-11 min-w-[343px] lg:w-[556px] py-[50px] px-5 md:px-0 border border-primary-1350 rounded-base shadow-table-cta bg-white">
-    <p className="self-stretch flex flex-col items-start gap-3 md:gap-[10px] md:px-8">
-      <span className="text-primary-300 text-[24px] leading-[36px] md:leading-[44px] tracking-[-0.48px] font-medium">Verifying Migration</span>
-      <span className="text-primary-1500 text-sm md:text-[16px] md:leading-[30px]">Please follow these steps to verify migration:</span>
-    </p>
-    <div className="max-w-[560px] flex-1 flex flex-col items-start gap-9 md:px-8">
-      {verifySteps.map((step, index) => (
-        <VerifyStep key={index} text={step.text} variant={step.variant} />
-      ))}
+const Right = () => {
+  const { locale } = useSystemFunctions();
+  const { gitText, subtitle, title, xText } = locale.newMigration.step4.right;
+
+  const verifySteps: VerifyStepProps[] = [
+    {
+      text: xText,
+      variant: 'x',
+    },
+    {
+      text: gitText,
+      variant: 'git',
+    },
+  ];
+
+  return (
+    <div className="flex flex-col item-center gap-11 min-w-[343px] lg:w-[556px] py-[50px] px-5 md:px-0 border border-primary-1350 rounded-base shadow-table-cta bg-white">
+      <p className="self-stretch flex flex-col items-start gap-3 md:gap-[10px] md:px-8">
+        <span className="text-primary-300 text-[24px] leading-[36px] md:leading-[44px] tracking-[-0.48px] font-medium">{title}</span>
+        <span className="text-primary-1500 text-sm md:text-[16px] md:leading-[30px]">{subtitle}</span>
+      </p>
+      <div className="max-w-[560px] flex-1 flex flex-col items-start gap-9 md:px-8">
+        {verifySteps.map((step, index) => (
+          <VerifyStep key={index} text={step.text} variant={step.variant} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default Right;
