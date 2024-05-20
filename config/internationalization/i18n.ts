c1b6969fa4cb818ed35de84cb2c506836e387253
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-20 13:58:29 +0100

    add french to locales

--- config/internationalization/i18n.ts
+++ config/internationalization/i18n.ts
@@ -1,6 +1,6 @@
 export const i18n = {
   defaultLocale: 'en',
-  locales: ['en', 'de', 'es'],
+  locales: ['en', 'de', 'es', 'fr'],
 } as const;
 
 export type Locale = (typeof i18n)['locales'][number];
