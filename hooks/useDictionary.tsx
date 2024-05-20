ffbaae53127b60f8b4902a0b21356947a922861c
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-20 13:59:00 +0100

    add french dictionaty to dictionaries

--- hooks/useDictionary.tsx
+++ hooks/useDictionary.tsx
@@ -5,6 +5,7 @@ export const dictionaries = {
   en: () => import('@/dictionaries/en.json').then((module) => module.default),
   de: () => import('@/dictionaries/de.json').then((module) => module.default),
   es: () => import('@/dictionaries/es.json').then((module) => module.default),
+  fr: () => import('@/dictionaries/fr.json').then((module) => module.default),
 };
 
 const useDictionary = () => {
