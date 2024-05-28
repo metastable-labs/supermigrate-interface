a62f454bd637b24df216e540aa145427644449d1
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-28 12:22:04 +0100

    feat: Add types for secondary earn page components

---
+++ views/earn/secondary/types.ts
@@ -0,0 +1,31 @@
+import { ReactElement } from 'react';
+
+interface InfoProps {
+  title: string;
+  value: number;
+  hasIcon?: boolean;
+  textRight?: boolean;
+}
+
+type BadgeType = 'primary' | 'secondary';
+type BadgeVariant = 'primary' | 'secondary' | 'tertiary' | 'mint' | 'plain';
+
+interface Badge {
+  text: string;
+  type?: BadgeType;
+  variant?: BadgeVariant;
+}
+
+interface ActionProps {
+  title: string;
+  titleAlt?: string;
+  titleBadge?: Badge;
+  subtitle?: string;
+  badges?: Badge[];
+  hasWarning?: boolean;
+  buttonText: string;
+  action: () => void;
+  icon?: ReactElement;
+}
+
+export type { InfoProps, ActionProps, Badge, BadgeType, BadgeVariant };
