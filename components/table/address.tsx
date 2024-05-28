3220c559b90cf92f44d3bc318a52e116c6864798
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-28 13:36:17 +0100

    refactor: Update Address component to use 'truncatedText' variable instead of 'truncateWallet'

--- components/table/address.tsx
+++ components/table/address.tsx
@@ -5,12 +5,12 @@ import { SecondaryCopyIcon } from '@/public/icons';
 import { AddressProps } from './types';
 
 const Address = ({ address }: AddressProps) => {
-  const truncateWallet = useTruncateText(address!, 5, 5);
+  const { truncatedText } = useTruncateText(address!, 5, 5);
   const copy = useCopy();
 
   return (
     <div className="flex items-start justify-start gap-1">
-      <span className="text-primary-300 text-sm font-medium">{truncateWallet}</span>
+      <span className="text-primary-300 text-sm font-medium">{truncatedText}</span>
       <SMClickAnimation onClick={() => copy(address!)}>
         <SecondaryCopyIcon />
       </SMClickAnimation>
