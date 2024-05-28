cd8d13f3e9bc084af8da165580cee92c6b601668
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-28 12:22:52 +0100

    refactor: Update EarnView component styling for step 1

--- views/earn/index.tsx
+++ views/earn/index.tsx
@@ -15,7 +15,7 @@ const EarnView = ({ lang }: LangParamProp) => {
   const [step, setStep] = useState(1);
   return (
     <SMContainer>
-      <div className={classNames('pb-20', { 'md:mt-14 lg:mt-[90px]': step === 0, 'mt-12': step === 1 })}>
+      <div className={classNames('pb-20', { 'md:mt-14 lg:mt-[90px]': step === 0, '-mt-10 md:mt-4 lg:mt-12': step === 1 })}>
         <AnimatePresence mode="popLayout">
           <motion.div key={step} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.3 }}>
             {steps[step]}
