ac0307ad4dc6594aa8353bfac702309690697198
Favour Nwajoko <chocorazzee@gmail.com>
2024-05-31 11:40:27 +0100

    refactor: Update Action component to include mobileTitle and mobileText in BadgeComponent

--- views/earn/secondary/action.tsx
+++ views/earn/secondary/action.tsx
@@ -2,6 +2,7 @@ import { SMButton } from '@/components';
 import { ActionProps, Badge, BadgeVariant } from './types';
 import { WarningIcon, MigrateLinkIcon, FlashIcon } from '@/public/icons';
 import classNames from 'classnames';
+import useTruncateText from '@/hooks/useTruncateText';
 
 const getIconColor = (variant?: BadgeVariant) => {
   switch (variant) {
@@ -18,14 +19,15 @@ const getIconColor = (variant?: BadgeVariant) => {
   }
 };
 
-const BadgeComponent = ({ text, type, variant }: Badge) => (
+const BadgeComponent = ({ text, type, variant, mobileText, fullWidth }: Badge) => (
   <div
-    className={classNames('py-1 pl-1 pr-2 flex  items-center justify-center gap-0.5 rounded-full text-xs md:text-[14px] md:leading-[21px] font-medium', {
+    className={classNames('py-1 pl-1 pr-2 flex  items-center justify-center gap-0.5 rounded-full text-xs md:text-[14px] md:leading-[21px] font-medium whitespace-nowrap', {
       'text-primary-4200 bg-primary-4500': variant === 'mint',
       'text-primary-200 bg-primary-150': variant === 'plain',
       'text-primary-2650 bg-primary-4450': variant === 'tertiary',
       'text-primary-4250 bg-primary-4400': variant === 'secondary',
       'text-primary-4300 bg-primary-4350': variant === 'primary',
+      'w-full md:w-auto': fullWidth,
     })}>
     {type === 'primary' && (
       <div className="min-w-fit">
@@ -37,17 +39,16 @@ const BadgeComponent = ({ text, type, variant }: Badge) => (
         <MigrateLinkIcon color={getIconColor(variant)} width={16} height={17} />
       </div>
     )}
-    {text}
+    <span className="block md:hidden">{mobileText}</span>
+    <span className="hidden md:block">{text}</span>
   </div>
 );
 
-const truncateTitle = (title: string, maxLength = 11) => {
-  return title.length > maxLength ? `${title.substring(0, maxLength)}...` : title;
-};
-
-const Action = ({ action, buttonText, icon, subtitle, title, badges, hasWarning, titleAlt, titleBadge }: ActionProps) => {
+const Action = ({ action, buttonText, icon, subtitle, title, badges, hasWarning, titleAlt, titleBadge, mobileTitle }: ActionProps) => {
+  const { truncatedText } = useTruncateText(title, 11, 5);
+  const truncateTitle = title.length > 22;
   return (
-    <div className="self-stretch rounded-base bg-white border border-primary-3450 p-6 flex-1 flex flex-col gap-10 items-stretch justify-center md:flex-row md:items-start md:justify-between">
+    <div className="self-stretch rounded-base bg-white border border-primary-3450 py-6 px-4 md:p-6 flex-1 flex flex-col gap-10 items-stretch justify-center md:flex-row md:items-start md:justify-between">
       <div className="flex flex-col gap-6 w-full">
         <div className="flex items-center justify-start space-x-5">
           {icon && icon}
@@ -58,7 +59,7 @@ const Action = ({ action, buttonText, icon, subtitle, title, badges, hasWarning,
                   'md:text-[20px] md:leading-[30px] text-primary-3400 tracking-[-0.4px] font-Bitform': !titleAlt,
                   'md:text-[20px] md:leading-[38px] text-primary-300 font-Bitform': titleAlt,
                 })}>
-                <p className="block lg:hidden">{truncateTitle(title)}</p>
+                <p className="block lg:hidden">{mobileTitle ? mobileTitle : truncateTitle ? truncatedText : title}</p>
                 <p className="hidden lg:block">{title}</p>
                 {titleAlt && <p className="text-primary-350 text-base font-Aeonik"> {titleAlt}</p>}
               </div>
@@ -79,7 +80,7 @@ const Action = ({ action, buttonText, icon, subtitle, title, badges, hasWarning,
         )}
 
         {hasWarning && (
-          <div className="py-3 px-6 flex items-center justify-center gap-1 self-stretch bg-primary-4150 rounded-xl text-primary-3400 text-xs md:text-[14px] md:leading-[21px]">
+          <div className="py-3 px-4 md:px-6 flex items-center justify-center gap-1 self-stretch bg-primary-4150 rounded-xl text-primary-3400 text-xs md:text-[14px] md:leading-[21px]">
             <div className="min-w-fit">
               <WarningIcon />
             </div>
